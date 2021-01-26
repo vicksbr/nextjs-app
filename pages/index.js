f971e1bf2afdeeab930f54794c307bcd1cbeba15
vicksbr <pedropuzzi@gmail.com>
2021-01-26 17:55:42 -0300

    feat: add link wrapper component using MUI link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Button from '@material-ui/core/Button';
+import { Button } from '@material-ui/core/Button';
 
 export default function Home() {
   return (
